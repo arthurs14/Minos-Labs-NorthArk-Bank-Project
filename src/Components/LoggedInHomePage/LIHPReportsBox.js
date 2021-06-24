edfa28abbef8f9224263d17a205c0229dab330c2
unknown <fnumilat@gmail.com>
2021-06-24 16:25:50 -0500

    Created and styled the home page for logged-in users.

---
+++ src/Components/LoggedInHomePage/LIHPReportsBox.js
@@ -0,0 +1,14 @@
+import './LoggedInHomePage.css'
+
+function LIHPReportsBox() {
+    return (
+      <div className="Reports-Box-Main-Box">
+          <button className="Reports-Box-Button">Expense Report</button>
+          <button className="Reports-Box-Button">Revenue Report</button>
+          <button className="Reports-Box-Button">Projection Report</button>
+      </div>
+    );
+  }
+  
+  export default LIHPReportsBox;
+  
---
+++ src/Components/LoggedInHomePage/TransactionsLog.js
@@ -0,0 +1,69 @@
+import './LoggedInHomePage.css'
+
+function TransactionsLog() {
+    return (
+      <div className="Trans-Log-Main-Box">
+          <div className="Trans-Log-Title">
+              <p>Transactions</p>
+          </div>
+          <div className="Trans-Log-Trans-Titles">
+              <p className="Trans-Log-Trans-Date-Title">Date</p>
+              <p className="Trans-Log-Trans-Descr-Title">Description</p>
+              <p className="Trans-Log-Trans-Amount-Title">Amount</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+          <div className="Trans-Log-Transactions">
+              <p className="Trans-Log-Date-Transaction">6/22/2021</p>
+              <p className="Trans-Log-Descr-Transaction">Coca Cola Stock</p>
+              <p className="Trans-Log-Amount-Transaction">$300</p>
+          </div>
+      </div>
+    );
+  }
+  
+  export default TransactionsLog;
+  
