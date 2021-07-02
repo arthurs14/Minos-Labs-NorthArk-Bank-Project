bf5c71579810a59d7418d6e8367d54c11216f143
unknown <fnumilat@gmail.com>
2021-07-02 17:59:58 -0500

    Created dummy data for Transactions Log based on the structure Sergio provided, made some changes to its styling and implemented a simple sign out function for the signout button.

---
+++ client/src/Components/LoggedInHomePage/DummyData.js
@@ -0,0 +1,54 @@
+const transactionsData = [
+    {
+      Date: "6/1/2021",
+      Name: "Apple",
+      Item: "ETF",
+      Category: "Technology",
+      Amount: 50
+    },
+    {
+      Date: "6/2/2021",
+      Name: "Tesla",
+      Item: "Stock",
+      Category: "Automation",
+      Amount: 150
+    },
+    {
+      Date: "6/3/2021",
+      Name: "Facebook",
+      Item: "Stock",
+      Category: "Technology",
+      Amount: 20
+    },
+    {
+      Date: "6/4/2021",
+      Name: "Walmart",
+      Item: "Stock",
+      Category: "Grocery",
+      Amount: 500
+    },
+    {
+      Date: "6/4/2021",
+      Name: "AAN",
+      Item: "Stock",
+      Category: "Grocery",
+      Amount: 400
+    },
+    {
+      Date: "6/5/2021",
+      Name: "AAW",
+      Item: "Stock",
+      Category: "Airline",
+      Amount: 30
+    },
+    {
+      Date: "6/6/2021",
+      Name: "ADC",
+      Item: "Stock",
+      Category: "Pharmacy",
+      Amount: 110
+    }
+  ];
+  
+  export default transactionsData;
+  
