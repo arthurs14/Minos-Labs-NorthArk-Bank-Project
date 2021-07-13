58603366cb5a89ab9cb102d493a75b81261bdde2
arthurs14 <arthurcs426@gmail.com>
2021-07-12 21:11:54 -0700

    added user actions and reducers for the central context state

---
+++ client/src/Components/UserContext/userActions.js
@@ -0,0 +1,8 @@
+/* Actions for logging an authenticated user in */
+export const AUTH = 'AUTH';
+export const LOGOUT = 'LOGOUT';
+
+/* 
+* Add actions for getting transaction data 
+* for a user below 
+*/
